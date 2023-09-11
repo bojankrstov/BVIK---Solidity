a1c5a696d7e1060b695c85c210540ae399f85e49
bojankrstov <80770532+bojankrstov@users.noreply.github.com>
2023-09-11 20:27:20 +0200

    Add files via upload

---
+++ Lab6/scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+    const FinkiNFT = await hre.ethers.getContractFactory("FinkiNFT");
+    const finkiNFT = await FinkiNFT.deploy();
+
+    await finkiNFT.deployed();
+
+    console.log("Finki NFT deployed to:", finkiNFT.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
